f75c640bea3f1ad419eef2cc73947d6ff60ef4db
Ali <aligondal8750@gmail.com>
2022-03-24 01:49:41 +0500

    first commit

--- app/src/main/java/com/example/standort/MainActivity.kt
+++ app/src/main/java/com/example/standort/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.standort
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.standort.databinding.ActivityMainBinding
+import com.example.standort.viewModel.appViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val vm=ViewModelProvider(this)[appViewModel::class.java]
+        vm.str.observe(this, Observer {
+            binding.checkTv.setText(it.toString())
+        })
+
     }
 }
